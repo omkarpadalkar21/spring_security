203ebfb5509e757caae0e5c94e5e8d37f54b20af
omkar <omkarpadalkar21@gmail.com>
2025-10-15 10:56:51 +0530

    Initial Config

---
+++ src/main/java/com/omkar/spring_security/HelloController.java
@@ -0,0 +1,12 @@
+package com.omkar.spring_security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping
+    public String greet(){
+        return "Hello";
+    }
+}
